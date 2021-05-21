28d3a101b4b1b504b8135ce19a15bfd4583ea870
Mustafa Muhammadi <mustafa.mohammadi@ut.ac.ir>
2021-05-21 10:08:24 +0430

    .

--- TSP_with_manual_Cities.m
+++ TSP_with_manual_Cities.m
@@ -1,5 +1,5 @@
-%% In this code, we illustrate the use of the linear topology for
-% the cluster units in a Kohonen self-ofganizing map to sole a classic
+%% In this code, we illustrate the use of linear topology for
+% the cluster units in a Kohonen self-organizing map to sole a classic
 % problem in constrained optimization, the so-called traveling salesman
 % problem(TSP). The aim of the TSP is to find a tour of a given set of
 % cities that is of minimum length. A tour consists of visiting each city
